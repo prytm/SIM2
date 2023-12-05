937a38c1d768b02f426af8558165d0d1f376432a
prytm <125776274+prytm@users.noreply.github.com>
2023-12-05 17:48:23 +0700

    Update tugas.php

--- tugas.php
+++ tugas.php
@@ -14,11 +14,12 @@
         <nav>
           <ul>
             <li><a href="#Home">Home</a></li>
-            <li><a href="#about-us-section">About us</a></li>
+            <li><a href="#about-us-section">About Us</a></li>
+            <li><a href="#our-team-section">Our Team</a></li>
           </ul>
         </nav>
       </header>    
-  <main>
+  <main style="margin-bottom:0px;">
     <div id ="Home" class = "overlay">
       <section class="content">
         <div class="text">
@@ -31,32 +32,50 @@
           <img src="foto/รูปถือถ้วยรางวัลถ้วยรางวัลทองคำถ้วยรางวัลระยิบระยับถ้วยรางวัลยกสูง PNG , ประจำปี, ราง.jpeg">
         </div>
       </section>
-      <div id="about-us-section" class="about-us-section">
+      <br><br>
+      <div id="about-us-section">
+        <br>
+        <h2 class="judul-about-us" style="color:black;;">About us</h2>
+          <div style="display: flex; justify-content: space-around; margin: 20px auto;">
+            <section style="flex: 1; margin: 10px; padding: 15px; border: 1px solid #ddd; border-radius: 8px; background-color: #fff; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+                <h2>Tentang Kami</h2>
+                <p style="text-align:justify;">Selamat datang di <span class="highlight">CloudCert</span>, solusi inovatif untuk penyimpanan sertifikat berbasis cloud save! Kami dengan bangga menghadirkan platform yang memungkinkan Anda menyimpan sertifikat dengan aman dan mudah, memberikan Anda akses instan di mana saja dan kapan saja. Website kami beroperasi dengan menggunakan teknologi cloud save terkini. Ini berarti sertifikat Anda tidak hanya aman dari risiko kehilangan fisik, tetapi juga dapat diakses secara instan melalui berbagai perangkat. Kami memanfaatkan infrastruktur cloud yang andal untuk menyediakan penyimpanan yang handal dan scalable.</p>
+            </section>
+
+            <section style="flex: 1; margin: 10px; padding: 15px; border: 1px solid #ddd; border-radius: 8px; background-color: #fff; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+                <h2>Harga dan Paket</h2>
+                <p>Kami menyadari bahwa setiap pencapaian dan prestasi memiliki nilai tak ternilai. Oleh karena itu, kami menawarkan layanan penyimpanan sertifikat berbasis cloud save dengan harga yang terjangkau.</p>
+                <p>Harga Layanan: <span class="highlight">Rp 5.000,- (untuk penggunaan seumur hidup)</span></p>
+            </section>
+          </div>
+    </div>
+
+      <div id="our-team-section">
         <br><br>
-        <h2 class="judul-about-us">About Us</h2>
-        <p>Kami adalah mahasiswa Universitas Diponegoro program studi Statistika Fakultas Sains dan Matematik</p>
+        <h2 class="judul-about-us" style="color:#fff;">Our Team</h2>
+        <p style="text-align:center; color:#fff;">Kami adalah mahasiswa Universitas Diponegoro program studi Statistika Fakultas Sains dan Matematik</p>
         <section class="about-us">
-          <div class="card">
+          <div class="card" style="padding-bottom: 20px;">
             <img src="foto/WhatsApp Image 2023-11-15 at 21.08.35.jpeg" alt="Nama Pertama">
             <h3>Priya Musyaffa Tammam</h3>
-            <p>Tangerang</p>
+            <a href="https://wa.link/3qozi0" style="font-weight:bold; width:150px; height:30px; background-color:red; color:white; text-decoration: none; border-radius: 20px; text-align: center; border:none;"><button>Contact me</button></a>
           </div>
-          <div class="card">
+          <div class="card" style="padding-bottom: 20px;">
             <img src="foto/WhatsApp Image 2023-11-15 at 21.21.43.jpeg" alt="Nama Kedua">
             <h3>Vikri Haikal</h3>
             <p>Solok selatan</p>
           </div>
-          <div class="card">
+          <div class="card" style="padding-bottom: 20px;">
             <img src="foto/WhatsApp Image 2023-11-15 at 21.17.55.jpeg" alt="Nama Ketiga">
             <h3>Prayuganingtyas E.S</h3>
             <p>Solo</p>
           </div>
-          <div class="card">
+          <div class="card" style="padding-bottom: 20px;">
             <img src="foto/WhatsApp Image 2023-11-15 at 21.33.19.jpeg" alt="Nama Keempat">
             <h3>Nyoman Dyah Primasari</h3>
             <p>Bali</p>
           </div>
-          <div class="card">
+          <div class="card" style="padding-bottom: 20px;">
             <img src="foto/WhatsApp Image 2023-11-15 at 21.33.47.jpeg" alt="Nama Kelima">
             <h3>Luna Citra Alifa</h3>
             <p>Solo</p>
@@ -67,7 +86,7 @@
   </main>
 
     <footer>
-      <p>&copy; 2023 SertifikatKu. Hak Cipta Dilindungi.</p>
+      <p>&copy; 2023 CloudCert. Hak Cipta Dilindungi.</p>
     </footer>
 </body>
 </html>
